a9a973c733479821a60ebfa0f2bbad1c9f6428b2
Pedro <pedrosrvd@al.insper.edu.br>
2022-05-26 11:22:11 -0300

    finalizando inicio socket.io

--- server/index.js
+++ server/index.js
@@ -17,7 +17,14 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`)
-})
+  socket.on("join_room",(data)=>{
+    socket.join(data);
+  });
+  socket.on("send_message",(data)=>{
+    socket.to(data.room).emit("receive_message",data);
+  });
+  
+});
 
 server.listen(3001, () => {
     console.log("SERVER IS RUNNING")
